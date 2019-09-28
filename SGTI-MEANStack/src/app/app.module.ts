ae5605db36c4856a0f6f2b976dd34f0b8c007371
SpikeAsimov <hualterio@gmail.com>
2019-09-28 18:28:12 -0300

    add AdminLTE 2 + dashboard

--- SGTI-MEANStack/src/app/app.module.ts
+++ SGTI-MEANStack/src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NgxPopper } from 'angular-popper';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent,
+    WrapperComponent,
+   
+   
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPopper
   ],
   providers: [],
   bootstrap: [AppComponent]
