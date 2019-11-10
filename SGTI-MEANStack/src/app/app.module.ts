a491e5da45d5b550d9642f09202ee726054706f9
SpikeAsimov <hualterio@gmail.com>
2019-11-10 17:27:06 -0300

    se elimino el import de wrapper, del app.module

--- SGTI-MEANStack/src/app/app.module.ts
+++ SGTI-MEANStack/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 import { NgxPopper } from 'angular-popper';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { WrapperComponent } from './components/wrapper/wrapper.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { SigninComponent } from './components/user/signin/signin.component';
 import { SignupComponent } from './components/user/signup/signup.component';
@@ -20,7 +18,6 @@ import { SignupComponent } from './components/user/signup/signup.component';
     NavbarComponent,
     SidebarComponent,
     DashboardComponent,
-    WrapperComponent,
     FooterComponent,
     SigninComponent,
     SignupComponent,
