3b444972bb15530ce8237241925481041763d449
SpikeAsimov <hualterio@gmail.com>
2019-11-09 22:13:22 -0300

    Se creó la carpeta .shared, y se movieron los componentes navbar, sidebar y footer

--- SGTI-MEANStack/src/app/app.module.ts
+++ SGTI-MEANStack/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 import { NgxPopper } from 'angular-popper';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { WrapperComponent } from './components/wrapper/wrapper.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 
 
