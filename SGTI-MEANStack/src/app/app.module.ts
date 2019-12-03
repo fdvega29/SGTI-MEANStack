90de59f60ea259db26d8fe9ed51cedf9309bbeaa
Fernando <fervega2911@gmail.com>
2019-12-03 20:48:45 -0300

    Rutas desde adminlte signin y signup

--- SGTI-MEANStack/src/app/app.module.ts
+++ SGTI-MEANStack/src/app/app.module.ts
@@ -1,15 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 import { NgxPopper } from 'angular-popper';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+//Components
 import { SigninComponent } from './components/user/signin/signin.component';
 import { SignupComponent } from './components/user/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 
+
+
+const appRoutes :Routes = [
+
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+
+
+];
 
 
 @NgModule({
@@ -26,9 +49,11 @@ import { SignupComponent } from './components/user/signup/signup.component';
 
   ],
   imports: [
+    [RouterModule.forRoot(appRoutes)],
     BrowserModule,
     NgxPopper
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
