523250f315badc9cc052c85535d66af6e24d9cf3
spikeasimov <hualterio@gmail.com>
2019-12-04 21:28:03 -0300

    se agrega routing para app y se edita app.module

--- SGTI-MEANStack/src/app/app.module.ts
+++ SGTI-MEANStack/src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgxPopper } from 'angular-popper';
 import { RouterModule, Routes } from '@angular/router';
-//Components
+// Components
 import { SigninComponent } from './components/user/signin/signin.component';
 import { SignupComponent } from './components/user/signup/signup.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
-
-
-
 
 const appRoutes :Routes = [
 
@@ -34,7 +33,6 @@ const appRoutes :Routes = [
 
 ];
 
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -44,15 +42,14 @@ const appRoutes :Routes = [
     FooterComponent,
     SigninComponent,
     SignupComponent,
-   
-   
-
   ],
   imports: [
     [RouterModule.forRoot(appRoutes)],
     BrowserModule,
-    NgxPopper
-  ],
+    NgxPopper,
+    CommonModule,
+    AppRoutingModule
+  ]
   exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
