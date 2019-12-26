b3986961370176a2e8ac21dc7cbf7d7ae61dd9a2
SpikeAsimov <hualterio@gmail.com>
2019-12-26 20:48:21 -0300

    se agrego formGroup para Admin

--- SGTI-MEANStack/src/app/components/user/signup/signup.component.ts
+++ SGTI-MEANStack/src/app/components/user/signup/signup.component.ts
@@ -2,16 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { UsersModule } from 'src/app/components/user/usersModel/users/users.module';
 import { UserServiceService } from 'src/app/components/user/userService/users.service';
 import { FormControl, FormGroup } from '@angular/forms';
+import { AdminModule } from '../usersModel/adminModel/admin.module';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
-  providers: [UsersModule]
+  providers: [UsersModule, AdminModule]
 })
 export class SignupComponent implements OnInit {
 
-  createFormGroup(){
+  createFormGroupUser() {
     return new FormGroup({
       apellido: new FormControl(''),
       nombre: new FormControl(''),
@@ -21,16 +22,29 @@ export class SignupComponent implements OnInit {
     });
   }
 
-  signupForm: FormGroup;
+  createFormGroupAdmin() {
+    return new FormGroup({
+      usuario: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl('')
+    });
+  }
+
+  signupFormUser: FormGroup;
+  signupFormAdmin: FormGroup;
 
   constructor(private usersService: UserServiceService) {
-      this.signupForm = this.createFormGroup();
+      this.signupFormUser = this.createFormGroupUser();
+      this.signupFormAdmin = this.createFormGroupAdmin();
    }
+
+
 
   ngOnInit() {}
 
   onSaveForm(form): void{
-    console.log(this.signupForm.value);
+    console.log(this.signupFormUser.value);
+    console.log(this.signupFormAdmin.value);
   }
 
 }
--- SGTI-MEANStack/src/app/components/user/userService/users.service.ts
+++ SGTI-MEANStack/src/app/components/user/userService/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UsersModule } from '../usersModel/users/users.module';
+import { AdminModule } from '../usersModel/adminModel/admin.module';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class UserServiceService {
   readonly URL_API = ('http://localhost:3000/api/users');
 
   constructor(private http: HttpClient) {
-      this.selectedUser = new UsersModule(); 
+      this.selectedUser = new UsersModule();
    }
 
    //Metodos HTTP
@@ -33,4 +34,32 @@ export class UserServiceService {
    deleteUser(User: UsersModule){
     return this.http.delete(this.URL_API + `/${User._id}`);
   }
+}
+
+export class AdminServiceService {
+  selectedUser: AdminModule;
+  admin: AdminModule[];
+  readonly URL_API = ('http://localhost::3000/api/admin');
+
+  constructor(private http: HttpClient) {
+    this.selectedUser = new AdminModule();
+  }
+     //Metodos HTTP
+
+     getAdmin(){
+      return this.http.get(this.URL_API);
+    }
+
+    postAdmin(Admin: AdminModule){
+      return this.http.post(this.URL_API, Admin);
+    }
+
+    putAdmin(Admin: AdminModule){
+      return this.http.put(this.URL_API + `/${Admin._id}`, Admin);
+    }
+
+    deleteAdmin(Admin: AdminModule){
+     return this.http.delete(this.URL_API + `/${Admin._id}`);
+   }
+
 }
