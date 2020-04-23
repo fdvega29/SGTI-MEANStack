f21d4bafc27bbe1f25cf408d2ae14d9be200c29b
Walter Arroyo <hualterio@gmail.com>
2020-04-22 22:03:15 -0300

    agrega contenido de nuevo-tramite.component y js del SmartWizard

---
+++ SGTI-MEANStack/src/assets/dist/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+
+  //Smart-Wizard
+  $('#stepwizard').smartWizard({
+    theme: 'dots',
+    transitionEffect: 'slide',
+    transitionSpeed: '400',
+    selected: 2,
+    lang: { next: 'Siguiente', previous: 'Anterior'}
+  });
+
+});
+
