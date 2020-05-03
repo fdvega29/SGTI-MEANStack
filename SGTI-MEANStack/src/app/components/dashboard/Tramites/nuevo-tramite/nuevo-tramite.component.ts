e013fbb82bb09a1175ddd27c964b84d776e3065e
Spike <hualterio@gmail.com>
2020-05-03 20:34:05 -0300

    fix bug en guia-tramite.component y nuevo-tramite.component

--- SGTI-MEANStack/src/app/components/dashboard/Tramites/nuevo-tramite/nuevo-tramite.component.ts
+++ SGTI-MEANStack/src/app/components/dashboard/Tramites/nuevo-tramite/nuevo-tramite.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $;
+
 @Component({
   selector: 'app-nuevo-tramite',
   templateUrl: './nuevo-tramite.component.html',
@@ -10,6 +12,14 @@ export class NuevoTramiteComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //Smart-Wizard
+    $('#stepwizard').smartWizard({
+      theme: 'dots',
+      transitionEffect: 'slide',
+      transitionSpeed: '400',
+      selected: 0,
+      lang: { next: 'Siguiente', previous: 'Anterior'}
+    });
   }
 
 }
