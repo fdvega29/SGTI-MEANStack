df8b2d5ecfca03166ad0190c0398f9f5db2a3263
Spike <hualterio@gmail.com>
2020-05-25 22:42:33 -0300

    crea service sidebar y agrega menuUsuario

---
+++ SGTI-MEANStack/src/app/components/shared/sidebar/sidebar.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  menuUsuario: any = {
+    elementos: [
+          {
+            titulo: 'Perfil de Usuario',
+            icono: 'fa fa-user-circle-o',
+            submenu: [
+              {titulo: 'Perfil', url: '/dashboard/ver-perfil'},
+              {titulo: 'Editar perfil', url: '/dashboard/editar-perfil'},
+              {titulo: 'Cambiar contraseña', url: '/dashboard/cambiar-password'},
+            ]
+          },
+          {
+            titulo: 'Trámites',
+            icono: 'fa fa-file-text-o',
+            submenu: [
+              { titulo: 'Nuevo trámite', url:'/dashboard/nuevo-tramite'},
+              { titulo: 'Guia de Uso', url:'/dashboard/guia-tramite'},
+              { titulo: 'Mis trámites', url:'/dashboard/mis-tramites'},
+            ]
+          },
+          {
+            titulo: 'Acerca de',
+            icono: 'fa fa-info-circle',
+            submenu: [
+              { titulo: 'Información del Sistema', url:'/dashboard/informacion'},
+              { titulo: 'Preguntas frecuentes', url:'/dashboard/preguntas'}
+            ]
+          }
+        ]
+      }
+;
+
+
+
+
+  constructor() {
+  }
+}
