3b1a1af4279137a1c6274839dcc17ce03470b333
Spike <hualterio@gmail.com>
2020-07-02 23:26:20 -0300

    probando nuevas funciones en guards.ts

--- SGTI-MEANStack/src/app/components/core/guards/guards.ts
+++ SGTI-MEANStack/src/app/components/core/guards/guards.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AutenticacionService } from '../../services/autenticacion.service';
+import {UsuarioService} from "../../services/usuario.service";
+import {sessionUser} from "../../models/session.module";
 
 
 @Injectable({
@@ -13,7 +15,15 @@ import { AutenticacionService } from '../../services/autenticacion.service';
 // Creamos nuestra clase e implementamos CanActive
 export class AuthGuard implements CanActivate {
 
-  constructor(public authService: AutenticacionService, public router: Router) {}
+  usuario : sessionUser;
+  roleUsuario: string;
+
+  constructor(public authService: AutenticacionService, public router: Router, public usuarioService : UsuarioService) {
+    this.usuario = this.usuarioService.getCurrentUser();
+    this.roleUsuario = JSON.stringify(this.usuario['roles']);
+    //Elimina "" del JSON p/ hacer comparacion
+    this.roleUsuario = this.roleUsuario.replace(/['"]+/g, '');
+  }
 
   canActivate() {
 
@@ -23,6 +33,25 @@ export class AuthGuard implements CanActivate {
     }
     return true;
 
+    if (this.roleUsuario == 'USER_ROLE'){
+      this.router.navigate(['dashboard/principal']);
+      return false;
+    }
+    return true;
+
+  }
+
+  soyAdmin() {
+    this.usuario = this.usuarioService.getCurrentUser();
+    this.roleUsuario = JSON.stringify(this.usuario['roles']);
+    //Elimina "" del JSON p/ hacer comparacion
+    this.roleUsuario = this.roleUsuario.replace(/['"]+/g, '');
+
+    if (this.roleUsuario != 'ADMIN_ROLE'){
+      this.router.navigate(['dashboard/principal']);
+      return false;
+    }
+    return true;
   }
 
 }
