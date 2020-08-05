c5f9654fa9b3bb431d7e10a78f3966f1bafb1182
ikrak <hualterio@gmail.com>
2020-08-05 19:06:49 -0300

    actualizaciones para Heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/sgti'));
+app.get('/', function (req, res){
+  res.sendFile(path.join(__dirname+ '/dist/sgti/index.html'));
+})
