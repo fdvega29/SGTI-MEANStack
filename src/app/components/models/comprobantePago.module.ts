e08ec157affe7aa4c1978a9d62b1db3ef90076c1
Fernando <ferveg64@gmail.com>
2020-09-11 01:41:08 -0300

    Queda implementado mercado pago

--- src/app/components/models/comprobantePago.module.ts
+++ src/app/components/models/comprobantePago.module.ts
@@ -5,15 +5,17 @@ export class comprobantePago{
     public tramite: String;
     public importe: Number;
     public idOperacion: String;
+    public idColeccion: String;
     public estado: String;
 
-    constructor(_id = '', fecha = '', usuario = '', tramite = '', importe = 0, idOperacion = '', estado = ''){
+    constructor(_id = '', fecha = '', usuario = '', tramite = '', importe = 0, idOperacion = '', idColeccion = '', estado = ''){
         this._id = _id;
         this.fecha = fecha;
         this.usuario = usuario;
         this.tramite = tramite;
         this.importe = importe;
         this.idOperacion = idOperacion;
+        this.idColeccion = idColeccion;
         this.estado = estado;
     }
 };
