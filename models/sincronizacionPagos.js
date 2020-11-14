b4ae491072bcfd3ac1f7cbcea20b3ee3dc1bccb3
Fernando <ferveg64@gmail.com>
2020-11-14 12:45:25 -0300

    Se agrega campo fecha en modelo de datos sincronizacion pagos

--- models/sincronizacionPagos.js
+++ models/sincronizacionPagos.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const sincronizacionPago = new Schema({
     usuario: { type: Schema.Types.ObjectId, ref: 'Users'},
     totProcesados: {type: String},
-    totActualizados: {type: String}
+    totActualizados: {type: String},
+    fecha: new Date()
 },
 {
     timestamps: true
